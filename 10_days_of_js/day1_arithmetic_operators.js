c84d9ea170901d589b701b5e5f442d12f410ff74
Rashad <mkrashad25@gmail.com>
2020-07-27 15:03:56 +0200

    Created 10 days of js

---
+++ 10_days_of_js/day1_arithmetic_operators.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function getPerimeter(length, width) {
+  let perimeter;
+  // Write your code here
+  perimeter = (length + width) * 2
+  return perimeter;
+}
+
---
+++ 10_days_of_js/day1_functions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function main() {
+    if (n <= 1) {
+        return 1
+    }
+    else {
+        return n * factorial(n - 1)
+    }
+}
