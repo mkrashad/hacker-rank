10c4b02dd4966ae0ca7c5516c276d9bafdbba64b
Rashad <mkrashad25@gmail.com>
2020-07-31 21:45:27 +0200

    Added 3 solutions

---
+++ 10_days_of_js/throw3.js
@@ -0,0 +1,13 @@
+function isPositive(a) {
+  if (a > 0) {
+    return "YES"
+  }
+  else if (a === 0) {
+    throw Error("Zero Error")
+  }
+  else {
+    throw Error("Negative Error")
+  }
+}
+
+isPositive([2, 0, 6])
